9f489439500914c2d0f098527e335efa2b155ec6
manoj <manoj@inflolabs.com>
2019-07-26 15:40:23 -0400

    home

--- pages/home.vue
+++ pages/home.vue
@@ -1,38 +1,80 @@
 <template>
-<v-container>
-  <div
-    style="position: relative;"
-  >
-  <v-app>
-  <v-toolbar
-      color="teal lighten-3"
-      >
+  <div class="homediv">
+    <div class="postdiv">
+      <v-textarea solo name="input-7-4" label="Whats on your mind??"></v-textarea>
+      <v-btn class="postbtn">POST</v-btn>
+    </div>
 
-        <v-toolbar-title>Home page</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
-
-      <v-icon
-        large
-        color="grey darken-1"
-      >
-        account_box
-      </v-icon>
-         <v-icon
-        large
-        color="grey"
-      >
-        mail
-      </v-icon>
-    <v-avatar color="indigo">
-      <v-icon dark>account_circle</v-icon>
-    </v-avatar>
-  ></v-toolbar>
-  </v-app>
-      </div>
-</v-container>
+    <v-timeline>
+      <v-timeline-item small fill-dot>
+        <v-card max-width="344" class="mx-auto">
+          <v-card-title>
+            <v-avatar color="indigo" size="25">
+              <!-- <span class="white--text headline">36</span> -->
+            </v-avatar>
+            <span class="username">Username</span>
+          </v-card-title>
+          <v-card-text>I'm card text</v-card-text>
+          <v-card-actions>
+            <v-btn text>Click</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-timeline-item>
+      <v-timeline-item small fill-dot>
+        <v-card max-width="344" class="mx-auto">
+          <v-card-title>
+            <v-avatar color="indigo" size="25">
+              <!-- <span class="white--text headline">36</span> -->
+            </v-avatar>
+            <span class="username">Username</span>
+          </v-card-title>
+          <v-card-text>I'm card text</v-card-text>
+          <v-card-actions>
+            <v-btn text>Click</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-timeline-item>
+      <v-timeline-item small fill-dot>
+        <v-card max-width="344" class="mx-auto">
+          <v-card-title>
+            <v-avatar color="indigo" size="25">
+              <!-- <span class="white--text headline">36</span> -->
+            </v-avatar>
+            <span class="username">Username</span>
+          </v-card-title>
+          <v-card-text>I'm card text</v-card-text>
+          <v-card-actions>
+            <v-btn text>Click</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.homediv {
+  margin: 50px 280px;
+  /* border: solid grey 0.5px; */
+}
+
+.postbtn {
+  position: absolute !important;
+  bottom: 29px;
+  right: 0;
+  background: #1976d2 !important;
+  color: #ffffff !important;
+}
+
+.postdiv {
+  position: relative;
+}
+
+.username {
+  margin: 0 10px;
+}
+</style>
